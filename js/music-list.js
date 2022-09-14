3024e954dbe397da7652d637568593c35b224fd7
sunny <sunnykumar118989@gmail.com>
2022-09-15 02:21:47 +0530

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,22 @@
+let allMusic = [
+  {
+    name: "Ae Dil ha Mushkil",
+    artist: "Arjit Singh",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Numb",
+    artist: "Khan Bgaini",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Kesariya",
+    artist: "Arjit Singh",
+    img: "music-3",
+    src: "music-3"
+  },
+ 
+ 
+];
